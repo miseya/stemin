947fa6a3d047d20ad6d55061d6431a27e81da12b
Seya <31957516+sglkc@users.noreply.github.com>
2025-01-20 15:02:00 +0700

    build(tsup): update global name
    build(package.json): fix esm import

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   entry: ['src/index.ts'],
   format: ['esm', 'iife', 'cjs'],
-  globalName: 'mpstemmer',
+  globalName: 'stemin',
   clean: true,
   outDir: 'dist',
   dts: true,
